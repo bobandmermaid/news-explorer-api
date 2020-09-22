8ee6f756ff6846f699204910a2c1289ebc6f4362
Ilya Podolyak <bobandmermaid@yandex.com>
2020-09-22 21:37:52 +0300

    fix router sign..

--- routes/auth.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const { createUserValid, loginValid } = require('../middlewares/validation');
-const { createUser, login } = require('../controllers/auth');
-
-router.post('/', loginValid, login);
-router.post('/', createUserValid, createUser);
-
-module.exports = router;
