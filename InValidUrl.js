7a414442d2c2559738503d81f5d3568f9db4a5a0
Ilya Podolyak <bobandmermaid@yandex.com>
2020-09-27 14:00:12 +0300

    fix invalidUrl

--- InValidUrl.js
+++ InValidUrl.js
@@ -2,10 +2,6 @@ const NotFoundError = require('./errors/not-found-err');
 const { NOT_FOUND } = require('./errors/messageError');
 
 // Обработка некорректного адреса
-module.exports = (req, res, next) => {
-  try {
-    throw new NotFoundError(NOT_FOUND);
-  } catch (error) {
-    next(error);
-  }
+module.exports = () => {
+  throw new NotFoundError(NOT_FOUND);
 };
